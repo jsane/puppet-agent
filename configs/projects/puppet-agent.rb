a208ec9162d640d6662e3e0841be11b5bdbab4fa
Ubuntu <ubuntu@10-32-168-26.rfc1918.puppetlabs.net>
2017-09-07 17:07:54 +0000

    (PA-1264) Changes to enable building puppet agent against system openssl libraries
    This is being done to allow using PA on FIPS enabled systems. This specific version
    forces building against system openssl since Jenkins does not support environment
    variables. When building against any un-supported platforms it will switch to
    linking against vendored openssl and not use system openssl.

--- configs/projects/puppet-agent.rb
+++ configs/projects/puppet-agent.rb
@@ -1,3 +1,17 @@
+
+
+def use_system_openssl?
+
+  # We cannot specify environment variables in Jenkins so hard coding it here
+  # for testing it thru CI pipes.
+  return true
+
+  # explicitly catch the expected "don't use this" values
+  # return false if ENV["USE_SYSTEM_OPENSSL"] =~ /false|f|0/i
+  # explicitly cast the environment variable to a true Boolean otherwise
+  # !!ENV["USE_SYSTEM_OPENSSL"]
+end
+
 project "puppet-agent" do |proj|
   platform = proj.get_platform
 
@@ -252,7 +266,26 @@ project "puppet-agent" do |proj|
   end
   proj.component "ruby-shadow" unless platform.is_aix? || platform.is_windows?
   proj.component "ruby-augeas" unless platform.is_windows?
-  proj.component "openssl"
+
+  if use_system_openssl?
+    if platform.name =~ /^el-(6|7)-.*/ || platform.name =~ /^fedora-f(24|25).*/
+    #  if (platform.is_el? || platform.is_fedora?)
+      # Currently we plan to support linking against system openssl
+      # only on these platforms.
+      proj.setting(:vendor_openssl, "no")
+    else
+      # We cannot fail for Ubuntu on Jenkins so temporarily just warning and proceeding
+      warn "Currently linking against system openssl is supported only on Enterprise Linux 6,7 or Fedora 24, 25 platforms"
+      warn "Using packaged openssl instead"
+      # fail "Currently linking against system openssl is supported only on Enterprise Linux 6,7 or Fedora 24, 25 platforms"
+      proj.setting(:vendor_openssl, "yes")
+      proj.component "openssl"
+    end
+  else
+    proj.setting(:vendor_openssl, "yes")
+    proj.component "openssl"
+  end
+
   proj.component "puppet-ca-bundle"
   proj.component "libxml2" unless platform.is_windows?
   proj.component "libxslt" unless platform.is_windows?
