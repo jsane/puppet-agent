9f20a4294f89e92fb79c75c1f81ee69ca8450647
Jayant Sane <ghjsane@gmail.com>
2017-10-02 10:22:03 -0700

    (PA-1580) Enable testing of system openssl linked puppet agent on rhel7 platforms
    The install script in all relevant components including puppet-agent repo
    needs to be updated so it upgrades the version of openssl package on rhel7 platforms.
    RHEL7 images used by CI/VMpooler uses an old openssl version, compared to that on
    Centos7 which is where PA is built on,  which causes agent setup and tests to fail.
    This has been preventing PA built against openssl to be tested in CI pipelines
    which require one test target to be at least RHEL7.
    
    Following components have been updated with above changes that puppet-agent now
    refers to:
      - puppet-agent
      - puppet
      - hiera
      - facter
      - pxp-agent
    
    The openssl package version to upgrade to can be specified using the following
    environment variable: RHEL7_OPENSSL_VERSION. If not specified it defaults to
    a version that is known to work with PA built against system openssl (on centos7).

--- acceptance/setup/aio/pre-suite/010_Install.rb
+++ acceptance/setup/aio/pre-suite/010_Install.rb
@@ -8,6 +8,22 @@ step "Install puppet-agent..." do
     :default_action => 'gem_install'
   }
   agents.each do |agent|
+    next if agent == master
+    
+    # Move the openssl libs package to a newer version on redhat platforms
+    use_system_openssl = ENV['USE_SYSTEM_OPENSSL']
+
+    if use_system_openssl && agent[:platform].match(/(?:el-7|redhat-7)/)
+      rhel7_openssl_version = ENV['RHEL7_OPENSSL_VERSION']
+      if rhel7_openssl_version.to_s.empty?
+        # Fallback to some default is none is provided
+        rhel7_openssl_version = "openssl-1.0.1e-51.el7_2.4.x86_64"
+      end
+      on(agent, "yum -y install " +  rhel7_openssl_version) 
+    else
+      step "Skipping upgrade of openssl package... (" + agent[:platform] + ")"
+    end
+
     if ENV['TESTING_RELEASED_PACKAGES']
       # installs both release repo and agent package
       install_puppet_agent_on(agent, opts)
